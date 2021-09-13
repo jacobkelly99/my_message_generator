9b1217b1df5f8d34035aea555794bed93c6abfce
jacobkelly99 <jacobkelly99@outlook.com>
2021-09-13 23:19:57 +0100

    Add code

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-function randomTrumpTweet() = {
+function randomTrumpTweet() {
     let subject = ['Bernie Sanders', 'The Undertaker', 'Obama', 'Hilary', 'Tucker Carlson', 'Ted Cruz', 'Mitt Romney', 'Ronald McDonald', 'Chuck E Cheese', 'David Dobrik', 'Jacob Kelly']
     let action = ['receiving the lowest viewing figures since records began.', 'begging people for more funding.', 'unable to pleasure their partner in bed.', 'calling my friends asking for a meeting with me.', 'losing their house in Florida.', 'eating too much mac and cheese.', 'unable to walk in a straight line.']
     let exclamation = ['Sad!', 'Loser!', 'What a joke!', 'Pathetic!', 'Lame!', 'I never liked them!', 'I called it!']
@@ -7,5 +7,6 @@ function randomTrumpTweet() = {
     let chosenAction = Math.floor(Math.random()*action.length)
     let chosenExclamation = Math.floor(Math.random()*exclamation.length)
     let randomMessage = `${subject[chosenSubject]} is ${action[chosenAction]} ${exclamation[chosenExclamation]}`
+    console.log(randomMessage);
     return randomMessage;
 }
